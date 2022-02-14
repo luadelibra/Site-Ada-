81a6a3250ad43b40f975c5484e9474f245411938
Luana Dantas <97513517+luadelibra@users.noreply.github.com>
2022-02-14 19:34:25 +0000

    Initial commit

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+document.getElementById("botaoEnviar").addEventListener("click", validarFormulario)
+
+function validarFormulario(){
+  if(document.getElementById("nome").value != "" &&   
+    document.getElementById("email").value != ""){
+      alert("Prontinho! Você receberá as novidades por email.")
+  }else{
+    alert("Por favor, preencha os campos nome e email!")
+  }
+}
